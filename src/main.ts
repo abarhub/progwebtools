9b8e503374cb818bc5fe89a33e8865a04a2e0eac
abarhub <abarhub@gmail.com>
2022-04-04 13:39:24 +0200

    génération du site

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'codemirror/mode/javascript/javascript';
-// import 'codemirror/mode/xml/xml';
+ import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/markdown/markdown';
